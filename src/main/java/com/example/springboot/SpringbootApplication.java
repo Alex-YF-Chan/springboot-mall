610db3c9d886d72dfb1a993136c4a62cc2d730b8
AlexFFCC <m3zo0926@gmail.com>
2023-08-19 16:00:00 +0800

    修改連線設定

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,9 +1,14 @@
 package com.example.springboot;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySources;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SpringbootApplication {
 
     public static void main(String[] args) {
